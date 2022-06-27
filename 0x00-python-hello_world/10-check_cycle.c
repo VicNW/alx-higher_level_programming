49e76620860adb33a34365cb3b77ebe9c00e139f
Nwosu Victor <nwosuvictor54@gmail.com>
2022-06-27 21:33:36 +0400

    first commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks for loops in a singly listed link
+* @list: pointer to singly linked list
+* Return 0: cycle does not exists
+* Return: cycle exists
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list;
+	listint_t *slow = list;
+
+	while (fast && slow && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	if(fast == slow)
+	{
+		return 1;
+	}
+return 0;
+}
