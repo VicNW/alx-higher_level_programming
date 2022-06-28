43c6987b292f86bddb03ff6099242ceddbed8ac4
Nwosu Victor <nwosuvictor54@gmail.com>
2022-06-28 15:26:09 +0400

    task1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,5 @@ if last_digit > 5:
     print('Last digit of ', number, ' is ', n, ' and is greater than 5')
 if last_digit == 0:
     print('Last digit of ', number, ' is ', n, ' and is 0')
-if last_digit < 6 and != 0:
+if last_digit < 6:
     print('Last digit of ', number, ' is ', n, ' and is less than 6 and not 0')
