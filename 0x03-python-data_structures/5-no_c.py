21846fca46ac0e4b02c64a378c84a368aaf60af9
Nwosu Victor <nwosuvictor54@gmail.com>
2022-07-04 16:49:06 +0400

    first commit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def no_c(my_string)i:
+
+'''This function removes every occurance of c 
+both capital and small letters'''
+    
+    new_string = ''
+    for char in my_string:
+        if i != 'c' and i != 'C':
+            new_string += char
+    return new_string
---
+++ 0x03-python-data_structures/practice/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if sentence == '':
+        return None
+    return length = len(sentence) first_char = sentence[0]
