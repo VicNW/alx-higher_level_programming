dd4ac89b7e7996b034857ef0104bae47b624987a
Nwosu Victor <nwosuvictor54@gmail.com>
2022-07-04 17:26:02 +0400

    second major commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-def no_c(my_string)i:
+def no_c(my_string):
 
-'''This function removes every occurance of c 
+    '''This function removes every occurance of c 
 both capital and small letters'''
     
     new_string = ''
     for char in my_string:
-        if i != 'c' and i != 'C':
+        if char != 'c' and char != 'C':
             new_string += char
     return new_string
