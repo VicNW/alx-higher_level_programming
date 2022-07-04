55809510ec56af0e56b584089844e5a7172a901a
Nwosu Victor <nwosuvictor54@gmail.com>
2022-07-04 18:21:26 +0400

    task_6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for elements in row:
-            print('{:d}'.format(elements), end ='' if elements == row[-1] else ' ')
+        for col in row:
+            print('{:d}'.format(col), end='' if col == row[-1] else ' ')
         print()
