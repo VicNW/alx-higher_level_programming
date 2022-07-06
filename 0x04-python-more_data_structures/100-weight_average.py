51520dbc5c6f7681060d1accfe6ee96158a2ed96
Nwosu Victor <nwosuvictor54@gmail.com>
2022-07-06 23:46:21 +0400

    added return 0 incase my_list does not exist or the lenght is 0

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list):
+    if my_list and len(my_list):
         num_ = 0
         denom_ = 0
         for tup in my_list:
             num += (tup[0] * tup[1])
             denom += (tup[1])
         return (num/denom)
+    return 0
