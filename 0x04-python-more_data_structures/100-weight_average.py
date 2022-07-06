35713d37c15409ce9f46a2ef6f76b0532fa6427a
Nwosu Victor <nwosuvictor54@gmail.com>
2022-07-06 23:48:43 +0400

    I corrected the local variable names

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list and len(my_list):
-        num_ = 0
-        denom_ = 0
+        num = 0
+        denom = 0
         for tup in my_list:
             num += (tup[0] * tup[1])
             denom += (tup[1])
