88b5145d28a812e5a053dee059dee4cb81ad0558
Nwosu Victor <nwosuvictor54@gmail.com>
2022-07-19 02:15:03 +0400

    .

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (TypeError, ValueError) as err:
-        print("Exception: {}".format(err), file=sys.stderr)
+    except (ValueError, TypeError) as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
     else:
         return True
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from __future__ import print_function
-import sys
-
 def safe_function(fct, *args):
     try:
-        result = fct(*args)
-    except Exception as err:
-        print("Exception: {}".format(err), file=sys.stderr)
+        r = fct(*args)
+        return r
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
         return None
-    else:
-        return result
